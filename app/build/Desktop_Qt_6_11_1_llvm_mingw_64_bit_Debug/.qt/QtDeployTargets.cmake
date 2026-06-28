3464abc97df20b024ed05e1375d0fd6173e30464
LeslieKogi <lesliekogi@gmail.com>
2026-06-28 19:14:28 +0300

    Initial commit

---
+++ app/build/Desktop_Qt_6_11_1_llvm_mingw_64_bit_Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_app_FILE C:/Users/lesli/Desktop/projects/Boomba-FM-Music-Player/app/build/Desktop_Qt_6_11_1_llvm_mingw_64_bit_Debug/app.exe)
+set(__QT_DEPLOY_TARGET_app_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_app_RUNTIME_DLLS C:/Qt/6.11.1/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/llvm-mingw_64/bin/Qt6Core.dll)
