da49ed2a08b140f513a335bb61fb84c19c91c23a
Rita-Gatimu <rita.gatimu@strathmore.edu>
2026-06-29 20:55:40 +0300

    Created MusicLibrary class

--- app/musiclibrary.h
+++ app/musiclibrary.h
@@ -1,10 +1,24 @@
 #ifndef MUSICLIBRARY_H
 #define MUSICLIBRARY_H
 
-class musicLibrary
+#include <QVector>
+#include "song.h"
+
+class MusicLibrary
 {
 public:
-    musicLibrary();
+    MusicLibrary();
+
+    void addSong(const Song &song);
+    void removeSong(int index);
+    Song getSong(int index) const;
+    QVector<Song> getAllSongs() const;
+QVector<Song> searchByArtist(const QString &artist) const;
+QVector<Song> searchByGenre(const QString &genre) const;
+    int getSongCount() const;
+
+private:
+    QVector<Song> m_songs;
 };
 
 #endif // MUSICLIBRARY_H
