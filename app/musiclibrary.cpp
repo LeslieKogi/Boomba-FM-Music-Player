c0a41698903e1527ad002e68ec885c364c139a62
Rita-Gatimu <rita.gatimu@strathmore.edu>
2026-06-29 20:57:51 +0300

    Implemented MusicLibrary functions

--- app/musiclibrary.cpp
+++ app/musiclibrary.cpp
@@ -1,3 +1,38 @@
 #include "musiclibrary.h"
 
-musicLibrary::musicLibrary() {}
+musicLibrary::musicLibrary() 
+{
+    // Constructor implementation
+}
+void musicLibrary::addSong(const Song &song) 
+{
+    m_songs.append(song);
+}
+QVector<Song> musicLibrary::getAllSongs() const
+{
+    return m_songs;
+}
+QVector<Song> musicLibrary::searchByArtist(const QString &artist) const
+{
+    QVector<Song> result;
+    for (const Song &song : m_songs) 
+    {
+        if (song.getArtist() == artist) 
+        {
+            result.append(song);
+        }
+    }
+    return result;
+}
+QVector<Song> MusicLibrary::searchByGenre(const QString &genre) const
+{
+    QVector<Song> result;
+    for (const Song &song : m_songs) 
+    {
+        if (song.getGenre() == genre) 
+        {
+            result.append(song);
+        }
+    }
+    return result;
+}
