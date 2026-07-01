f2bcf9532958176525d78d27b58a43157c42cf5e
Bridgit <bridgit.mokua@strathmore.edu>
2026-07-01 20:17:50 +0300

    added a song

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,22 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QAudioOutput>
+#include <QMediaPlayer>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     MainWindow w;
     w.show();
-    return QApplication::exec();
+
+    QMediaPlayer *player = new QMediaPlayer;
+    QAudioOutput *output = new QAudioOutput;
+
+    player->setAudioOutput(output);
+    player->setSource(QUrl::fromLocalFile("C:/Users/bridg/Downloads/time.mp3"));
+    output->setVolume(0.8);
+    player->play();
+
+    return a.exec();
 }
