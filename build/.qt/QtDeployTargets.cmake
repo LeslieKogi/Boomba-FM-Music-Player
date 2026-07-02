5a0cf6c0e881bcab9cfb65b7d4a0ea6ec9c54261
Bridgit <bridgit.mokua@strathmore.edu>
2026-07-02 04:02:42 +0300

    debugging

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_app_FILE C:/Users/bribg/OneDrive/Documents/Boomba-FM-Music-Player/build/app.exe)
+set(__QT_DEPLOY_TARGET_app_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_app_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
