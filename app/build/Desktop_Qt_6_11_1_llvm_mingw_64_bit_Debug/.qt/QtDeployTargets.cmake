aff99f50c3d3409a929bd64d81fd251aa42e6e6d
LeslieKogi <lesliekogi@gmail.com>
2026-07-05 14:47:00 +0300

    Stop tracking build folder

--- app/build/Desktop_Qt_6_11_1_llvm_mingw_64_bit_Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_app_FILE C:/Users/lesli/Desktop/projects/Boomba-FM-Music-Player/app/build/Desktop_Qt_6_11_1_llvm_mingw_64_bit_Debug/app.exe)
-set(__QT_DEPLOY_TARGET_app_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_app_RUNTIME_DLLS C:/Qt/6.11.1/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/llvm-mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.11.1/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/llvm-mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.1/llvm-mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.11.1/llvm-mingw_64/bin/Qt6Core.dll)
